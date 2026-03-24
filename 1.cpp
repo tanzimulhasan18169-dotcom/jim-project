2b5c7adab5eec9fde1ad410596f6c003dbccbd0a
tanzimulhasan18169-dotcom <tanzimulhasan18169@gmail.com>
2026-03-25 04:19:31 +0600

    Initial commit: Added all project files

---
+++ 1.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    FILE *fp;
+    char input[100];
+    int i = 0, flag = 1;
+
+    fp = fopen("input.txt", "r");
+
+    if(fp == NULL)
+    {
+        printf("File not found\n");
+        return 1;
+    }
+
+    fscanf(fp, "%s", input);
+
+    while(input[i] != '\0')
+    {
+        if(!isdigit(input[i]))
+        {
+            flag = 0;
+            break;
+        }
+        i++;
+    }
+
+    if(flag == 1)
+        printf("Numeric Constant\n");
+    else
+        printf("Not Numeric\n");
+
+    fclose(fp);
+    return 0;
+}
